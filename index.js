91703d2c0efc27fe8e12c8d0bffcfbbc86dce2bc
Shakar Bakr <5h4k4r.b4kr@gmail.com>
2023-10-10 15:59:33 +0300

    Create PR in another repo
    Signed-off-by: Shakar Bakr <5h4k4r.b4kr@gmail.com>

--- index.js
+++ index.js
@@ -1,22 +1,73 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
 const { Octokit } = require('@octokit/rest')
+const { context } = require('@actions/github');
 
 
-const octokit = new Octokit({
-  auth: core.getInput('github-token')
-})
+// const octokit = new Octokit({
+//   auth: core.getInput('github-token')
+// })
+try {
 
-// Get the branch name from
-const branch = github.context.payload.ref.replace('refs/heads/', '').replace('refs/tags/', '')
-console.log("branch: ", branch)
-// if branch is equal to main log a message else if it's dev log another message. if it's from a tag log a third message
-if (branch === 'main') {
-  console.log('This is the main branch')
-}
-else if (branch === 'dev') {
-  console.log('This is the dev branch')
-}
-else if (branch.startsWith('v')) {
-  console.log('This is a tag')
+  const currentRepo = github.context.payload.repository.full_name.split('/')[1];
+  const branchOrTagName = github.context.payload.ref.replace('refs/heads/', '').replace('refs/tags/', '');
+  const env = branchOrTagName.startsWith('v') ? 'prod' : branchOrTagName;
+  const imageTag = core.getInput('imageTag', { required: true })
+
+
+  console.log("branch: ", branch);
+
+  try {
+    const gh = github.getOctokit(core.getInput('github-token'))
+
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+
+    const branchNameOnGitOpsRepo = `update-${currentRepo}-${env}`; // update-dms-dev
+
+    await gh.rest.repos.getBranch({
+      owner,
+      repo,
+      branch: branchNameOnGitOpsRepo
+    })
+
+    core.setFailed(`Branch ${branchNameOnGitOpsRepo} already exists on ${owner}/${repo}`)
+    return;
+  } catch (error) {
+    // do nothing
+  }
+
+  core.info(`Branch ${branchNameOnGitOpsRepo} does not exist on ${owner}/${repo}`)
+  core.info(`Creating branch ${branchNameOnGitOpsRepo} on ${owner}/${repo}`)
+  core.info(`Owner: ${owner}`)
+  core.info(`Repo: ${repo}`)
+  core.info(`Branch: ${branchNameOnGitOpsRepo}`)
+  core.info(`Commit: ${context.sha}`)
+
+  await gh.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${branchNameOnGitOpsRepo}`,
+
+    sha: context.sha
+  })
+
+
+
+  // Create a PR to merge the branch into main
+  const pr = await gh.rest.pulls.create({
+    owner,
+    repo,
+    title: `Update ${currentRepo}'s image tag to ${imageTag}`,
+    head: branchNameOnGitOpsRepo,
+    base: 'main',
+    body: `Update ${currentRepo}'s image tag to ${imageTag}`
+  })
+
+  core.setOutput('prUrl', pr.data.html_url)
+  core.setOutput('branchName', branchNameOnGitOpsRepo)
+  core.setOutput('branchUrl', `https://github.com/${owner}/${repo}/tree/${branchNameOnGitOpsRepo}`)
+
+} catch (error) {
+  core.setFailed(error.message)
 }
