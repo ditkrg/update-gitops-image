036c98fb325d1983e8aebda41ca70169b98c6071
Shakar Bakr <5h4k4r.b4kr@gmail.com>
2023-10-10 11:05:50 +0300

    Add some logs
    Signed-off-by: Shakar Bakr <5h4k4r.b4kr@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello World')
+const github = require('@actions/github')
+const core = require('@actions/core')
+const { Octokit } = require('@octokit/rest')
+
+
+const octokit = new Octokit({
+  auth: core.getInput('github-token')
+})
+
+// Get the branch name from the event payload
+const branch = github.context.payload.pull_request.head.ref
+
+// if branch is equal to main log a message else if it's dev log another message. if it's from a tag log a third message
+if (branch === 'main') {
+  console.log('This is the main branch')
+}
+else if (branch === 'dev') {
+  console.log('This is the dev branch')
+}
+else if (branch.startsWith('v')) {
+  console.log('This is a tag')
+}
